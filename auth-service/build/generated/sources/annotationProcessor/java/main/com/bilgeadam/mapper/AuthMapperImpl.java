b14eb239c9d464b144f5b81ed1c6e25e6a932863
veyselkayhan <veyselkayhan13@gmail.com>
2023-11-26 17:35:03 +0300

    Last update

---
+++ auth-service/build/generated/sources/annotationProcessor/java/main/com/bilgeadam/mapper/AuthMapperImpl.java
@@ -0,0 +1,94 @@
+package com.bilgeadam.mapper;
+
+import com.bilgeadam.dto.request.RegisterRequestDto;
+import com.bilgeadam.dto.request.UserCreateRequestDto;
+import com.bilgeadam.dto.response.RegisterResponseDto;
+import com.bilgeadam.rabbitmq.model.RegisterMailModel;
+import com.bilgeadam.rabbitmq.model.RegisterModel;
+import com.bilgeadam.repository.entity.Auth;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-24T02:19:49+0300",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 17.0.7 (Oracle Corporation)"
+)
+@Component
+public class AuthMapperImpl implements AuthMapper {
+
+    @Override
+    public Auth fromRegisterRequestToAuth(RegisterRequestDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Auth.AuthBuilder<?, ?> auth = Auth.builder();
+
+        auth.username( dto.getUsername() );
+        auth.password( dto.getPassword() );
+        auth.email( dto.getEmail() );
+
+        return auth.build();
+    }
+
+    @Override
+    public RegisterResponseDto fromAuthToRegisterResponse(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        RegisterResponseDto.RegisterResponseDtoBuilder registerResponseDto = RegisterResponseDto.builder();
+
+        registerResponseDto.id( auth.getId() );
+        registerResponseDto.username( auth.getUsername() );
+        registerResponseDto.activationCode( auth.getActivationCode() );
+
+        return registerResponseDto.build();
+    }
+
+    @Override
+    public UserCreateRequestDto fromAuthToUserCreateRequestDto(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        UserCreateRequestDto.UserCreateRequestDtoBuilder userCreateRequestDto = UserCreateRequestDto.builder();
+
+        userCreateRequestDto.authId( auth.getId() );
+        userCreateRequestDto.username( auth.getUsername() );
+        userCreateRequestDto.email( auth.getEmail() );
+
+        return userCreateRequestDto.build();
+    }
+
+    @Override
+    public RegisterModel fromAuthToRegisterModel(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        RegisterModel.RegisterModelBuilder registerModel = RegisterModel.builder();
+
+        registerModel.authId( auth.getId() );
+        registerModel.username( auth.getUsername() );
+        registerModel.email( auth.getEmail() );
+
+        return registerModel.build();
+    }
+
+    @Override
+    public RegisterMailModel fromAuthToRegisterMailModel(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        RegisterMailModel.RegisterMailModelBuilder registerMailModel = RegisterMailModel.builder();
+
+        registerMailModel.email( auth.getEmail() );
+        registerMailModel.username( auth.getUsername() );
+        registerMailModel.activationCode( auth.getActivationCode() );
+
+        return registerMailModel.build();
+    }
+}
---
+++ user-service/build/generated/sources/annotationProcessor/java/main/com/bilgeadam/mapper/UserMapperImpl.java
@@ -0,0 +1,61 @@
+package com.bilgeadam.mapper;
+
+import com.bilgeadam.dto.request.UserCreateRequestDto;
+import com.bilgeadam.rabbitmq.model.RegisterModel;
+import com.bilgeadam.repository.entity.UserProfile;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-26T17:21:01+0300",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 17.0.7 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserProfile fromCreateRequestToUser(UserCreateRequestDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        UserProfile.UserProfileBuilder<?, ?> userProfile = UserProfile.builder();
+
+        userProfile.authId( dto.getAuthId() );
+        userProfile.username( dto.getUsername() );
+        userProfile.email( dto.getEmail() );
+
+        return userProfile.build();
+    }
+
+    @Override
+    public UserProfile fromRegisterModelToUserProfile(RegisterModel model) {
+        if ( model == null ) {
+            return null;
+        }
+
+        UserProfile.UserProfileBuilder<?, ?> userProfile = UserProfile.builder();
+
+        userProfile.authId( model.getAuthId() );
+        userProfile.username( model.getUsername() );
+        userProfile.email( model.getEmail() );
+
+        return userProfile.build();
+    }
+
+    @Override
+    public UserCreateRequestDto fromRegisterModelToUserCreateDto(RegisterModel model) {
+        if ( model == null ) {
+            return null;
+        }
+
+        UserCreateRequestDto.UserCreateRequestDtoBuilder userCreateRequestDto = UserCreateRequestDto.builder();
+
+        userCreateRequestDto.authId( model.getAuthId() );
+        userCreateRequestDto.username( model.getUsername() );
+        userCreateRequestDto.email( model.getEmail() );
+
+        return userCreateRequestDto.build();
+    }
+}
